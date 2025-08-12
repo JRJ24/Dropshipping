088296d28de72208c0ce39cbc043330e522ddc69
Jonaifry <rjstudios24012006@gmail.com>
2025-08-12 14:53:01 -0400

    Droshipping

---
+++ models/users.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+    name:{
+        type: String,
+        required: true
+    },
+    photo:{
+        type: String,
+        required: false
+    }, 
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    phone:{
+        type: String,
+        required: true,
+        unique: true
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
---
+++ src/app.js
@@ -0,0 +1,43 @@
+require('dotenv').config();
+const express = require('express');
+const mongoose = require('mongoose');
+const session = require('express-session');
+const router = require('../routes/routes'); // Adjust the path as necessary
+
+const app = express();
+const PORT = process.env.PORT || 4000;
+
+// Use environment variable for MongoDB connection string
+mongoose.connect(process.env.DB_URI);
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', () => {
+    console.log('Connected to MongoDB');
+});
+
+//middleware
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+app.use(session({
+    secret: 'your_secret_key',
+    saveUninitialized: true,
+    resave: false, 
+}));
+
+app.use((req, res, next) => {
+    res.locals.message = req.session.message;
+    delete req.session.message;
+    next();
+});
+
+//configuring view engine
+app.set('view engine', 'ejs');
+
+app.use('/', router); // Use the router for handling routes
+
+app.use('/upload', express.static('upload'));
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port http://localhost:${PORT}`);
+});

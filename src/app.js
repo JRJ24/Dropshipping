5d26f7ae60fb1dd8c846580c9423e8b1d8a980b7
Cristian Luna <cristianlunarosario08@gmail.com>
2025-08-14 10:36:57 -0400

    Guardando mis datos antes de actualizarlos haciendo merge con main

--- src/app.js
+++ src/app.js
@@ -8,7 +8,7 @@ const app = express();
 const PORT = process.env.PORT || 4000;
 
 // Use environment variable for MongoDB connection string
-mongoose.connect(process.env.DB_URILocal);
+mongoose.connect(process.env.DB_URI);
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', () => {
@@ -39,8 +39,9 @@ app.set('view engine', 'ejs');
 
 app.use('/', router); // Use the router for handling routes
 //app.use('/ViewProduct', routerProduct);
-app.use('/upload', express.static('upload'));
-
+//app.use('/upload', express.static('upload'));
+const path = require('path');
+app.use('/upload', express.static(path.join(__dirname, '../upload')));// de esta fomra la descripcion de las imagenes son un poco mas descriptivas en lugar de solo la fecha y hora exacta, ya que posee el nombre de la imagen 
 app.listen(PORT, () => {
     console.log(`Server is running on port http://localhost:${PORT}`);
 });
